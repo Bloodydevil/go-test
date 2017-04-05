713343b7b102cb19a125a1fb2c76decaeabd3a86
Stefan Dieterle <stefan@dieterle.me>
2017-04-05 20:46:35 +0200

    fixed unicode bug

--- math/math.go
+++ math/math.go
@@ -5,7 +5,18 @@ import (
 	"math"
 )
 
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Printf("Now you have %g problems", math.Sqrt(7))
 	fmt.Println(math.Pi)
+	fmt.Println(add(42, 1337))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
--- string/string.go
+++ string/string.go
@@ -2,7 +2,7 @@ package string
 
 //Reverse Reverses a string and returns the output
 func Reverse(s string) string {
-	b := []byte(s)
+	b := []rune(s)
 	for i := 0; i < len(b)/2; i++ {
 		j := len(b) - i - 1
 		b[i], b[j] = b[j], b[i]
