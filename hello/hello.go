c1d9431889f765810db2e3511c7edb30ba9ebf8e
Stefan Dieterle <stefan@dieterle.me>
2017-04-05 15:25:50 +0200

    Blubb

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bloodydevil/string"
 )
 
 func main() {
 	fmt.Println(string.Reverse("This should print Hello world backwards!"))
+
+	fmt.Println("The time is", time.Now())
 }
